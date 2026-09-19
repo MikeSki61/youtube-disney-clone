a8be0e2a58aca643b8853d9db50befa6068e4c31
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-09-19 19:09:26 -0400

    installed react-redux errors

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,42 @@
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { auth, provider } from '../firebase';
+import {
+  selectUserName, 
+  selectUserPhoto, 
+  setUserLoginDetails,
+} from '../features/user/userSlice';
+
+
+
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const username = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
 
   const handleAuth = () => {
-    auth.signInWithPopup(provider).then((result) => {
-      console.log(result)
-    }).catch((error) => {
-      alert(error.message);
-    })
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
+
+  const setUser = (user) => {
+    dispatch(setUserLoginDetails({
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL,
+      })
+    );
+  };
+
+  
   return (
     <Nav>
       <Logo>
