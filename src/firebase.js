2a8191b80100c9aa2d539e1b6106739c540041a5
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-09-19 13:54:16 -0400

    added login functionality

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firerstore();
+const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 const storage = firebase.storage();
