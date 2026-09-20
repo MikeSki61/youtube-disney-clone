7c787763ac60080caac346b67772e8d53f7efc6f
Mike Kwiatkowsky <mikeski885@gmail.com>
2026-09-20 12:04:01 -0400

    lost background image

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ import { auth, provider } from '../firebase';
 import {
   selectUserName,
   selectUserPhoto,
+  setSignOutState,
   setUserLoginDetails,
 } from '../features/user/userSlice';
 
@@ -26,15 +27,22 @@ const Header = (props) => {
 
 
   const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        setUser(result.user);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth.signOut().then(() => {
+          dispatch(setSignOutState())
+          navigate("/home")
+      }).catch((err) => alert(err.message))
+    }
+};
 
   const setUser = (user) => {
     dispatch(setUserLoginDetails({
@@ -83,7 +91,13 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <userImg src={userPhoto} alt={userName} />
+          <SignOut>
+            <UserImg src="./images/karate.jpg" alt="KARATE" />
+            <DropDown>
+              <span onClick={handleAuth}>Sign Out</span>
+            </DropDown>
+          </SignOut>
+
         </>
       }
     </Nav>
@@ -200,8 +214,47 @@ const Login = styled.a`
   }
 `;
 
-const userImg = styled.img`
+const UserImg = styled.img`
   height: 100%;
+  width: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
